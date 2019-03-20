d29a86d2cc2f91be62fd4cb18b7923337e018a63
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-03-20 19:34:39 -0400

    Course Picker

--- 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
-class StudentEntryViewController: UIViewController {
+class StudentEntryViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+  
+    
+    
+    @IBOutlet weak var lblCourse: UILabel!
+    
+  
+    @IBOutlet weak var pickList: UIPickerView!
+    
+    var courseList = ["MADT","MODT", "CSD", "WADT"]
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.courseList.count
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.pickList.delegate = self
+        self.pickList.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lblCourse.text = "Course: \(self.courseList[self.pickList.selectedRow(inComponent:0)])"
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.courseList[row]
+    }
+    
+    
+
+    
 
 }
