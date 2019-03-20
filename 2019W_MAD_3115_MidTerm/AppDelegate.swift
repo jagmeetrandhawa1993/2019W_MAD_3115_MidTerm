e729b372a5a7e70453aff1c50292f2c9087330db
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-03-20 19:03:39 -0400

    Navigation to student entry VC

--- 2019W_MAD_3115_MidTerm/AppDelegate.swift
+++ 2019W_MAD_3115_MidTerm/AppDelegate.swift
@@ -18,7 +18,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    
+   /* private func splash(){
+        let splashVC = UIStoryboard.init(name: "LaunchScreen", bundle: nil)
+        let rootVC = splashVC.instantiateViewController(withIdentifier: "splash")
+        self.window?.makeKeyAndVisible()
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(dismissSplash), userInfo: nil, repeats: false)
+    }
+    @objc func dismissSplash()
+    {
+        let mainVC = UIStoryboard.init(name: "Main", bundle: nil)
+        let rootVC = mainVC.instantiateViewController(withIdentifier: "initController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+    }
+*/
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,63 +10,65 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var txtEmail: UITextField!
-    
-    @IBOutlet weak var txtPassword: UITextField!
-    
-    @IBOutlet weak var swRememberMe: UISwitch!
-    
-    @IBAction func btnLogin(_ sender: UIButton) {
-        
-        let email = txtEmail.text
-        let pwd = txtPassword.text
-        
-       if(email == "admin" && pwd == "admin@123")
-        {
-            print("Login Success..")
-            /*let userDefault = UserDefaults.standard
-            if (swRememberMe.isOn)
-            {
-                let UserDefault = UserDefaults.standard
-                UserDefault.set(email, forKey: "userEmail")
-                UserDefault.set(pwd, forKey: "userPassword")
- 
-            let sb = UIStoryboard(name: "Main", bundle: nil)
-            let entryVC = sb.instantiateViewController(withIdentifier: "entryVC") as! StudentEntryViewController
-            //entryVC.Email = email
-            self.navigationController?.pushViewController(entryVC, animated: true)
-            //self.present(homeVC, animated: true)
-        }*/
-        
-       }
- 
-        else{
-            
-            self.showAlert()
-            
-            
-        }
-        
-        
-    }
-    
-    
-    
-    func showAlert()
-    {
-        let alert = UIAlertController(title: "Alert", message: "<Invalid Email and Password>", preferredStyle: .alert)
-        
-        let actionDefault = UIAlertAction(title: "OK", style: .default, handler: nil)
-        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {(action:UIAlertAction) in
-            print("Cancel Click")
-        })
-        
-        alert.addAction(actionDefault)
-        alert.addAction(actionCancel)
-        
-        self.present(alert, animated: true)
-    }
- 
+      @IBOutlet weak var txtEmail: UITextField!
+     
+     @IBOutlet weak var txtPassword: UITextField!
+     
+     @IBOutlet weak var swRememberMe: UISwitch!
+     
+     @IBAction func btnLogin(_ sender: UIButton) {
+     
+     let email = txtEmail.text
+     let pwd = txtPassword.text
+     
+     
+     
+     if(email == "admin" && pwd == "admin@123")
+     {
+     print("Login Success..")
+     let userDefault = UserDefaults.standard
+     if (swRememberMe.isOn)
+     {
+     let UserDefault = UserDefaults.standard
+     UserDefault.set(email, forKey: "userEmail")
+     UserDefault.set(pwd, forKey: "userPassword")
+     
+     let sb = UIStoryboard(name: "Main", bundle: nil)
+     let entryVC = sb.instantiateViewController(withIdentifier: "entryVC") as! StudentEntryViewController
+     
+     //self.navigationController?.pushViewController(entryVC, animated: true)
+     self.present(entryVC, animated: true)
+     }
+     
+     }
+     
+     else{
+     
+     self.showAlert()
+     
+     
+     }
+     
+     
+     }
+     
+     
+     
+     func showAlert()
+     {
+     let alert = UIAlertController(title: "Alert", message: "<Invalid Email and Password>", preferredStyle: .alert)
+     
+     let actionDefault = UIAlertAction(title: "OK", style: .default, handler: nil)
+     let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {(action:UIAlertAction) in
+     print("Cancel Click")
+     })
+     
+     alert.addAction(actionDefault)
+     alert.addAction(actionCancel)
+     
+     self.present(alert, animated: true)
+     }
+     
  
 }
 
